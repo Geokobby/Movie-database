6efd2014547e1be3f63a9e51c5115118d09f0aa1
George Obeng-Boateng <geokobbymail@gmail.com>
2025-08-29 11:36:43 +0000

    react-movie-database

---
+++ movie-database/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center text-blue-700 text-3xl font-bold underline'>Hello World </h1>
+    </div>
+  )
+}
+
+export default App
