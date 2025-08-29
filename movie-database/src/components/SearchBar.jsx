333021f148a06cd02756f58cad63171b03cea7fc
George Obeng-Boateng <geokobbymail@gmail.com>
2025-08-29 21:15:08 +0000

    built the Search Bar

--- movie-database/src/components/SearchBar.jsx
+++ movie-database/src/components/SearchBar.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
 
 const SearchBar = () => {
   return (
-      <div className='   mx-10 shadow-lg'>
-          <div className='flex items-center place-content-around'>
-              <div>
-              <input type="text" placeholder='search' className='bg-gray-200 rounded-lg  my-2 px-3 '/>
+      <div className='   border-b'>
+          <div className='flex items-center place-content-around py-3'>
+              <div className='bg-white shadow-xl flex items-center rounded-xl pl-4 w-1/2'>
+          <FaSearch className='text-gray-400' />    
+          <input type="text" placeholder='Movie Search' className='bg-transparent font-bold w-full focus:outline-none  my-2 px-3' />
           </div>
            
-          <div className='flex'>
-              <h2 className=' mx-4 '>Review</h2>
-              <h2>Browse Movie</h2>
-          </div></div>
+          </div>
           
     </div>
   )
